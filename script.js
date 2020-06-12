a055feefe8a50e7d716d26970a5a1f68d602085d
Liubov Shakhova <shakhova_l90@mail.ru>
2020-06-12 15:27:48 +0200

    lesson06-hard

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 'use strict';
 
 let isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+
+    return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 const game = function() {
 
-  let n = Math.floor(Math.random() * 100) + 1;
-  console.log(n);
+let n = Math.floor(Math.random() * 100) + 1;
+console.log(n);
+let i = 1;
 
-  const condition = function(){ 
+const condition = function(){ 
     let question = prompt('Угадай число от 1 до 100');
 
-    if ( parseInt(question) > n) {
-        alert('Загаданное число меньше.');
+    if ( parseInt(question) > n && i !== 10) {
+        alert('Загаданное число меньше, осталось попыток ' + (10 - i));
+        i++;
         condition();
-    } else if (parseInt(question) < n) {
-        alert('Загаданное число больше');
+    } else if (parseInt(question) < n && i !== 10) {
+        alert('Загаданное число больше, осталось попыток ' + (10 - i));
+        i++;
         condition();
     } else if (parseInt(question) === n){
-        alert(' You are the Winner!');
+        let commit = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+        game();
+        if (!Boolean(commit)) {
+            alert('We are sorry. Hope to see you soon');
+            return;
+        }
     } else if (!isNumber(question) && question !== null) { 
         alert('Введи число!'); 
         condition();
     } else if (question === null) {
-          alert('We are sorry. Hope to see you soon'); 
+        alert('We are sorry. Hope to see you soon'); 
+    } else if ( i === 10) {
+        let commit = confirm('Попытки закончились, хотите сыграть еще?');
+        game();
+        if (!Boolean(commit)) {
+            alert('We are sorry. Hope to see you soon');
+            return;
+        }
     }
     };
     condition();
